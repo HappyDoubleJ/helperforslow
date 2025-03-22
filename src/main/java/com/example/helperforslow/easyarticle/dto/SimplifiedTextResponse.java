366298f558066020f3ea99f66a7f7cd2eadd1ce0
shjj9 <shjj9250@gmail.com>
2025-03-22 12:32:47 +0900

    2

--- src/main/java/com/example/helperforslow/easyarticle/dto/SimplifiedTextResponse.java
+++ src/main/java/com/example/helperforslow/easyarticle/dto/SimplifiedTextResponse.java
@@ -3,6 +3,7 @@ package com.example.helperforslow.easyarticle.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Getter
 @NoArgsConstructor
